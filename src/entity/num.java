39d925979e40871a4be1db9603a0e3fcfc657ed5
Cyc-InGitHub <8839712@qq.com>
2020-10-07 17:08:59 +0800

    Initial commit

---
+++ src/entity/num.java
@@ -0,0 +1,31 @@
+/**  
+* <p>Title: num.java</p>  
+* <p>Description: </p>  
+* <p>Copyright: Copyright (c) 2018</p>  
+* @author Cyc 
+* @date 2020年10月7日  
+* @version 1.0  
+
+*/  
+package entity;
+
+import java.util.ArrayList;
+
+/**  
+* <p>Title: num</p>  
+* <p>Description: </p>  
+* @author Cyc  
+* @date 2020年10月7日
+*/
+public class num {
+ArrayList<Integer>  list;
+
+public ArrayList<Integer> getList() {
+	return list;
+}
+
+public void setList(ArrayList<Integer> list) {
+	this.list = list;
+}
+
+}
